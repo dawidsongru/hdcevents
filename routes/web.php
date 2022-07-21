f0f42396eacfd40dc8d260a822a0932441bf645a
Dawidson Pereira <dawidsongru@gmail.com>
2022-07-21 04:19:59 -0300

    conhecendo o blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = "Dawidson";
+    $idade = 29;
+
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => "Programador"]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
 });
