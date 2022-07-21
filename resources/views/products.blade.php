0475567b4c991d62c176acbf6468b966ae7ab14b
Dawidson Pereira <dawidsongru@gmail.com>
2022-07-21 08:14:28 -0300

    Resgatando parâmetros de URL

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -3,5 +3,8 @@
 @section('title', 'HDC Events')
 
 @section('content')
-<h1>Tela de produtos</h1>
+    <h1>Tela de produtos</h1>
+    @if($busca != '')
+    <p>O usuário está buscando por: {{ $busca }}</p>
+    @endif
 @endsection
