8a9bcbf9c5750844fabaed6bdedc53247fa042c8
Dawidson Pereira <dawidsongru@gmail.com>
2022-07-21 16:28:51 -0300

    Utilizando o Eloquent do Laravel

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -4,26 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event;
+
 class EventController extends Controller
 {
     
     public function index() {
 
-        $nome = "Dawidson";
-        $idade = 44;
+        $events = Event::all();
     
-        $arr = [10,20,30,40,50];
-    
-        $nomes = ["Dawidson", "Maria", "João", "Saulo"];
-    
-        return view('welcome',
-            [
-                'nome' => $nome,
-                'idade' => $idade,
-                'profissao' => "Programador",
-                'arr' => $arr,
-                'nomes' => $nomes
-            ]);
+        return view('welcome',['events' => $events]);
 
     }
 
